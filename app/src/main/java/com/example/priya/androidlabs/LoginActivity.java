ba16509e2c70f535f5bb5b2b811e348e0f6941ed
priya <pate0417@algonquinlive.com>
2018-01-30 19:43:48 -0500

    initial Commit

--- app/src/main/java/com/example/priya/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/priya/androidlabs/LoginActivity.java
@@ -2,12 +2,42 @@ package com.example.priya.androidlabs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class LoginActivity extends Activity {
 
+   protected static final String ACTIVITY_NAME = "LoginActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME,"In onStart()");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(ACTIVITY_NAME,"In onResume()");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME,"In onPause()");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(ACTIVITY_NAME,"In onCreate()");
+    }
+    @Override
+    protected void 	onDestroy(){
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME,"In onDestroy()");
     }
 }
